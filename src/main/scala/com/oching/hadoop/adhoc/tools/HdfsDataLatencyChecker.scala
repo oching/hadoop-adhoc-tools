1b31cee8319ac5865b07aee894cdf8831a153a07
Deng Ching-Mallete <Maria.ChingMallete@cba.com.au>
2017-02-13 17:03:40 +1100

    added data latency checker that calculates the number (in %) of daily partitions that are delayed by X days + initial tests

---
+++ src/main/scala/com/oching/hadoop/adhoc/tools/HdfsDataLatencyChecker.scala
@@ -0,0 +1,128 @@
+//
+// Copyright 2017 Commonwealth Bank of Australia
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//        http://www.apache.org/licenses/LICENSE-2.0
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//   limitations under the License.
+
+package com.oching.hadoop.adhoc.tools
+
+import scala.collection.mutable.Map
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.hadoop.security.UserGroupInformation
+
+import org.joda.time.{DateTime, Days}
+
+import org.slf4j.LoggerFactory
+
+/**
+  * Calculates the latency of the data in HDFS.
+  *
+  * Expected parameters:
+  * - N, where N can be 1 to 12 (to indicate whether to calculate the latency for the last N months)
+  * - directory to be checked
+  * - partition format (year=yyyy/month=MM/day=dd or partition_date=yyyy-MM-dd
+  *
+  * To execute:
+  *
+  * java -jar hadoop-adhoc-tools-1.0-SNAPSHOT-jar-with-dependencies.jar 6 /prod/view/warehouse/sitecatalyst/nimbus/hit_data year=yyyy/month=MM/day=dd
+  */
+object HdfsDataLatencyChecker {
+
+  val HADOOP_CONF_DIR = "/etc/hadoop/conf"
+
+  val requiredJobArguments =
+    "\n[1-12]                 - indicates whether to calculate the latency for the last N months" +
+    "\n[/path/to/directory]   - directory in HDFS to be checked for data latency" +
+    "\n[partition format]     - format of the partition directories: [partition_year]=yyyy/[partition_month]=MM/[partition_day]=dd or [partition_dir_name]=yyyy-MM-dd"
+
+  val _LOG = LoggerFactory.getLogger(getClass)
+
+  @throws(classOf[Exception])
+  def main(args: Array[String]) = {
+
+    if (args.isEmpty || args.length != 3)
+      throw new Exception(s"Passed parameters does not match expected arguments. Please provide the following arguments: $requiredJobArguments")
+
+    val Array(lastNMonths, directoryPath, partitionFormat) = args
+//    validate(lastNMonths, partitionFormat)
+
+    _LOG.info(s"Starting check of data latency: $lastNMonths $directoryPath $partitionFormat")
+
+    // create Hadoop configuration
+    val config = new Configuration()
+    config.addResource(new Path(s"file://$HADOOP_CONF_DIR/core-site.xml"))
+    config.addResource(new Path(s"file://$HADOOP_CONF_DIR/hdfs-site.xml"))
+
+    config.set("fs.hdfs.impl", classOf[org.apache.hadoop.hdfs.DistributedFileSystem].getName)
+    config.set("fs.file.impl", classOf[org.apache.hadoop.fs.LocalFileSystem].getName)
+
+    // TODO: enable from args
+    // handle Kerberos authentication, use auth token already in the server
+    UserGroupInformation.setConfiguration(config)
+    UserGroupInformation.loginUserFromSubject(null)
+
+    val fs = FileSystem.get(config)
+    val partitions = partitionFormat.split("/")
+    val latencyMap: Map[String, Int] = Map()
+    val currentDate = DateTime.now
+    var startDate = currentDate.minusMonths(lastNMonths.toInt)
+    var ctr = 0
+
+    while (!currentDate.isEqual(startDate)) {
+      val partitionPath = new Path(buildPath(directoryPath, partitions, startDate))
+      var latestTs = 1L
+      val files = fs.globStatus(partitionPath)
+
+      _LOG.debug(s"Processing partition: partitionPath=$partitionPath")
+
+      files.foreach(file => {
+          _LOG.debug(s"Processing file: file=${file.getPath.getName} | modificationTime=${file.getModificationTime}")
+          if (file.getModificationTime > latestTs) latestTs = file.getModificationTime
+      })
+
+      _LOG.debug(s"latestTs=$latestTs | startDate=${startDate.getMillis}")
+
+      val latencyInDays = Days.daysBetween(startDate.withTimeAtStartOfDay(), new DateTime(latestTs).withTimeAtStartOfDay()).getDays.toString
+      latencyMap.put(latencyInDays, latencyMap.getOrElse(latencyInDays, 0) + 1)
+
+      _LOG.info(s"Latency for ${startDate.toString("yyyy-MM-dd")}: $latencyInDays")
+
+      ctr = ctr + 1
+      startDate = startDate.plusDays(1)
+    }
+
+    _LOG.info("Successfully gathered data latency..\n")
+
+    _LOG.info(s"Data latency in $directoryPath for the last $lastNMonths months:")
+
+    latencyMap.foreach(e => {
+      _LOG.debug(s"Latency map entry: key=${e._1} val=${e._2}")
+
+      _LOG.info(s"T - ${e._1}: ${((e._2.toDouble) / ctr.toDouble) * 100} %  (${e._2} out of $ctr days)")
+    })
+  }
+
+  @throws(classOf[Exception])
+  private def validate(lastNMonths: String, partitionFormat: String) = {
+    if (!"[1-12]".r.findFirstIn(lastNMonths).isDefined)
+      throw new Exception("Invalid Last N months argument. Possible values are 0-12")
+
+    if (!"([a-zA-Z_]=yyyy/[a-zA-Z_]=MM/[a-zA-Z_]=dd|[a-zA-Z_]=yyyy-MM-dd)".r.findFirstIn(partitionFormat).isDefined)
+      throw new Exception("Invalid partition format. Possible values are [partition_year]=yyyy/[partition_month]=MM/[partition_day]=dd or [partition_dir_name]=yyyy-MM-dd")
+  }
+
+  private def buildPath(basePath: String, partitions: Array[String], date: DateTime) =
+    basePath + "/" + partitions.map(p => {
+      val sepIdx = p.indexOf("=") + 1
+      p. substring(0, sepIdx) + date.toString(p.substring(sepIdx))
+    }).mkString("/") + "/*"
+}
---
+++ src/test/scala/com/oching/hadoop/adhoc/tools/HdfsDataLatencyCheckerSpec.scala
@@ -0,0 +1,93 @@
+//
+// Copyright 2017 Commonwealth Bank of Australia
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//        http://www.apache.org/licenses/LICENSE-2.0
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//   limitations under the License.
+
+package com.oching.hadoop.adhoc.tools
+
+import java.io.File
+
+import org.apache.hadoop.fs.{FileUtil, Path}
+import org.apache.hadoop.hdfs.{DistributedFileSystem, HdfsConfiguration, MiniDFSCluster}
+
+import org.junit.runner.RunWith
+
+import org.specs2.Specification
+import org.specs2.runner.JUnitRunner
+import org.specs2.specification.BeforeAll
+
+@RunWith(classOf[JUnitRunner])
+class HdfsDataLatencyCheckerSpec extends Specification with BeforeAll {
+  def is =
+    s2"""
+        An HdfsDataLatencyChecker
+          should throw an error if no arguments are supplied $testLatencyCheckMissingArguments()
+          should throw an error if the number of arguments supplied are more than expected $testLatencyCheckArgumentsMoreThanExpectedNumberOfArguments()
+      """
+
+  private var _fs: DistributedFileSystem = _
+  val baseDir = new File(".").getAbsolutePath
+  val hdfsSourcesDir = "/tmp/sources/"
+  lazy val fs: DistributedFileSystem = _fs
+  var hdfsURI: String = "hdfs://localhost:8020"
+
+  def testLatencyCheckMissingArguments() = {
+    HdfsDataLatencyChecker.main(Array[String]()) must throwA[Exception]
+  }
+
+  def testLatencyCheckArgumentsMoreThanExpectedNumberOfArguments() = {
+    HdfsDataLatencyChecker.main(Array[String]("1", "2", "3", "4")) must throwA[Exception]
+  }
+
+  // TODO: add test for invalid arguments
+  def testLatencyCheckInvalidArguments() = {
+
+  }
+
+  // partition: year=yyyy/month=MM/day=dd
+  def testLatencyCheckForLast6MonthsWithYearMonthDayPartition() = {
+
+  }
+
+  def testLatencyCheckForLast12MonthsWithYearMonthDayPartition() = {
+
+  }
+
+  // partition: partition_date=yyyy-MM-dd
+  def testLatencyCheckForLast6MonthsWithDatePartition() = {
+
+  }
+
+  def testLatencyCheckForLast12MonthsWithDatePartition() = {
+
+  }
+
+
+  override def beforeAll() = {
+    // setup a miniDFS cluster
+    val hdfsRootDir = new File(baseDir, s"/target/hdfs/data-latency-check").getAbsoluteFile()
+
+    FileUtil.fullyDelete(hdfsRootDir)
+
+    val conf = new HdfsConfiguration()
+    conf.set(MiniDFSCluster.HDFS_MINIDFS_BASEDIR, hdfsRootDir.getAbsolutePath())
+
+    val builder = new MiniDFSCluster.Builder(conf)
+    val hdfsCluster = builder.build()
+    hdfsURI = s"hdfs://localhost:${hdfsCluster.getNameNodePort()}"
+
+    // create HDFS paths
+    _fs = hdfsCluster.getFileSystem
+    val sourcesPath = new Path(hdfsSourcesDir)
+
+    fs.create(sourcesPath)
+  }
+}
